d1bf48b9eb05dbad41a437b1aa27ab6bf0925604
sprhawk <hongbo@yang.me>
2016-01-16 10:44:39 +0800

    added tutorial steps

--- TestOrge3D/Ogre/TutorialApplication.cpp
+++ TestOrge3D/Ogre/TutorialApplication.cpp
@@ -30,7 +30,25 @@ TutorialApplication::~TutorialApplication(void)
 void TutorialApplication::createScene(void)
 {
     // Create your scene here :)
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.7f, 0.7f, 0.7f));
+    mSceneMgr->setSkyBox(true, "Examples/SpaceSkyBox");
     
+    mSceneMgr->createLight("Light")->setPosition(75,75,75);
+    
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+    Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
+    Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    ogreNode->attachObject(ogreEntity);
+    
+    
+    Ogre::Light* light = mSceneMgr->createLight("MainLight");
+    light->setPosition(0, 0, 0);
+    
+    mCamera->setPosition(0, 0, 500);
+    
+    Ogre::Entity* ogreEntity2 = mSceneMgr->createEntity("ogrehead.mesh");
+    Ogre::SceneNode* ogreNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(84, 48, 0));
+    ogreNode2->attachObject(ogreEntity2);
 }
 
 
